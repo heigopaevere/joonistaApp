ea9dcaf816abaa3fad654996e309efa5fe2e34db
heigopaevere <heigopaevere@gmail.com>
2018-05-09 14:18:36 +0300

    _ _ _ _ _ _

--- app/src/main/java/com/example/opilane/joonistaapp/MainActivity.java
+++ app/src/main/java/com/example/opilane/joonistaapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.opilane.joonistaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
+    private ImageButton currPaint, drawBtn, eraseBtn, mewBtn, saveBtn;
+    private float smallBrush, mediumBrush, largeBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawingView = findViewById(R.id.drawing);
+        LinearLayout paintLayout = findViewById(R.id.paint_colors);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+    }
+
+    public void paintClicked(View view){
+
     }
 }
--- app/src/main/java/com/example/opilane/joonistaapp/drawingView.java
+++ app/src/main/java/com/example/opilane/joonistaapp/drawingView.java
@@ -1,7 +1,15 @@
 package com.example.opilane.joonistaapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,11 +17,82 @@ import android.view.View;
  */
 
 public class drawingView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xFF660000;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
     public drawingView(Context context, AttributeSet attrs) {
         super(context,attrs);
         setupDrawing();
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
     private void setupDrawing() {
+
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+
+        }
+        invalidate();
+        return true;
+    }
+    public void setColor(String newColor){
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+    public void setBrushSize(float newSize){
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, getResources().
+                getDisplayMetrics());
+                brushSize = PixelAmout;
+                drawPaint.setStrokeWidth(brushSize);
+    }
+    public void setErase(boolean isErase){
+        erase = isErase;
+        if (erase) drawPaint.setXfermode(new PorterDuffXfermode())
     }
 }
